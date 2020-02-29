e1e4783d5d6f145d4b2d19cb27850aceeed84379
Jvision <29251143+luyaxige@users.noreply.github.com>
2020-02-29 18:37:07 +0800

    [Linked List] removeDuplicateNodes

---
+++ src/com/luyaxige/solution/Solution.java
@@ -0,0 +1,16 @@
+package com.luyaxige.solution;
+
+import com.luyaxige.solution.linkedlist.LinkedList;
+import com.luyaxige.solution.linkedlist.ListNode;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        LinkedList linkedList = new LinkedList();
+
+        /**
+         * https://leetcode-cn.com/problems/remove-duplicate-node-lcci/submissions/
+         * */
+        linkedList.removeDuplicateNodes();
+    }
+}
